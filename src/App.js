0e5a92813208727ea4104ac5fb1e415d3289b9f0
BIT Student <classroom@bgit.rs>
2018-03-27 14:58:07 +0200

    App navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Header } from '../src/app/partials/Header'
+import { Footer } from './app/partials/Footer'
+import {Route,Switch} from 'react-router-dom'
+import{Main} from './Main'
 
 class App extends Component {
   render() {
-    return (<div>shone brat</div>)
+    return (
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route to='/' component={Main} />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    )
   }
 }
 
 export default App;
+
---
+++ src/app/partials/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../App.css';
+
+const Footer = () => {
+    return (
+        <footer className="page-footer">
+            <div className='footer'>
+                <div className="container">
+                    <div className="row">
+                    </div>
+                </div>
+                <div className="footer-copyright">
+                    <div className="container">
+                        © 2018 Copyright by: Team69
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    )
+}
+export { Footer };
