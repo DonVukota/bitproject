8338c552e356d566b331d89a70a8394cc468acca
DESKTOP-NCPH6QS\student <cedabrisic@gmail.com>
2018-03-28 10:07:51 +0200

    fixed footer

--- src/app/viewPostDetails/PostDetails.js
+++ src/app/viewPostDetails/PostDetails.js
@@ -2,7 +2,7 @@ import React, { Componenet } from 'react';
 import { VideoPost } from '../postFeed/VideoPost'
 import { TextPost } from '../postFeed/TextPost'
 import { ImagePost } from '../postFeed/ImagePost'
-import {Comment} from "./Comment"
+import { Comment } from "./Comment"
 import { InputComment } from './InputComment'
 
 
@@ -16,24 +16,25 @@ class ViewPostDetails extends React.Component {
     render() {
 
         return (
+            <main>
+                <div className="container">
+                    <div className="row">
 
-            <div className="container">
-                <div className="row">
+                        {/* if type of post is... */}
+                        <VideoPost />
 
-                    {/* if type of post is... */}
-                    <VideoPost />
-
-                    <InputComment/>
-
-
-
-                    <ul className="row">
-                        <Comment />
+                        <InputComment />
 
 
-                    </ul>
+
+                        <ul className="row">
+                            <Comment />
+
+
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </main>
 
 
         )
