095c332097e9b69dbedafdb9f41ee27de221bc05
DESKTOP-NCPH6QS\student <cedabrisic@gmail.com>
2018-03-30 15:43:28 +0200

    pocetak update profila

--- src/app/myProfile/MyProfilePage.js
+++ src/app/myProfile/MyProfilePage.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { fetchProfile } from '../services/fetchProfile'
-
+import M from "materialize-css"
 class MyProfilePage extends React.Component {
 
     constructor(props) {
@@ -35,6 +35,8 @@ class MyProfilePage extends React.Component {
     }
 
     componentDidMount() {
+      const modalButton= document.querySelector('.modal');
+        M.Modal.init(modalButton);
         this.getFeedData()
     }
 
@@ -50,7 +52,24 @@ class MyProfilePage extends React.Component {
                     <div className="row">
                         <div className="forum"> <i className="material-icons">forum</i><p className="postComment">Number of posts: {this.state.postsCount}</p></div>
                         <div className="comment"> <i className="material-icons">comment</i><p className="CommentNumber">Number of comments: {this.state.commentsCount}</p></div>
-                    </div></div>
+                    </div>
+                     <p className="waves-effect waves-light btn modal-trigger" href="#modal1">Edit Profile</p>
+                    </div>
+
+                 <div id="modal1" className="modal">
+                      <div className="modal-content">
+                             <h1>Update Profile</h1>
+                             <div className='row'>
+                             <p href="https://placeholder.com"><img src="http://via.placeholder.com/200x50"/></p>
+                             <button class="btn waves-effect waves-light" type="submit" name="action">UPLOAD PHOTO
+                                <i class="material-icons right">send</i>
+                            </button>
+                            </div>
+                      </div>
+                  <div className="modal-footer">
+                       <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+                 </div>
+  </div>
 
             </main>
 
