026ff934b8f79f2dad29e4c96f85ceb24a42f883
Cedomir Brisic <36138040+CedomirBrisic@users.noreply.github.com>
2018-04-27 09:06:38 +0200

    audio

--- src/app/postFeed/FeedPage.js
+++ src/app/postFeed/FeedPage.js
@@ -70,8 +70,6 @@ class FeedPage extends React.Component {
     render() {
         return (
             <main>
-                <audio id="audio" autoPlay loop src="thunder.mp3" />
-                <audio id="audio" autoPlay loop src="kise.mp3" />
                 <FilterFeed showFiltered={this.showFiltered} />
                 <div className=" container mainFeed ">
                     <div className="row">
